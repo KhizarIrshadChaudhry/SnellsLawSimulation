d261cb6be0d8f166239712b7896f2d34744456af
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2025-03-11 11:10:35 +0100

    Fikset bugs. Bug: kun defineret i vinklerne [-45;45]

--- src/main.js
+++ src/main.js
@@ -7,23 +7,44 @@ function setup(){
 let c = 299792458; //Lysets hastighed
 
 function indskudslys(v1=0){
-    let  x = tan(v1)*(windowWidth/2-windowWidth/8);
+    //omregning af indskudsvinkel v1 til koordinat
+    let y = sin(v1)*(windowWidth/2-windowWidth/4);
+
+    text(v1, width*2/8, 50)
+    
+    push();
     stroke("red");
-    line(windowWidth/8, (windowHeight/2)+x, windowWidth/2, windowHeight/2);
+    strokeWeight(2);
+    line(windowWidth/4, (windowHeight/2)+y, windowWidth/2, windowHeight/2);
+    pop();
 }
-function udskudslys(){
+let v1 = -45; //indskudsvinkel v1
+let b1 = 1; //brydningsindeks luft
+let b2 = 1.5; //brydningsindeks luft
+
+function udskudslys(v1){
+    //beregning af udskudsvinkel v2
+    v2 = asin(sin(v1)*b1/b2);
+    text(v2, width*5/8, 50)
+    //omregning af udskudsvinkel v2 til koordinat
+    let  x = -sin(v2)*(windowWidth/2-windowWidth/4);
+    push();
     stroke("darkred")
-    line(windowWidth*4/8, windowHeight/2, windowWidth*7/8, windowHeight/2);
+    strokeWeight(2);
+    line(windowWidth*4/8, windowHeight/2, windowWidth*3/4, windowHeight/2+x);
+    pop();
 }
 
 function draw(){
-    indskudslys(0);
-    udskudslys();
+    indskudslys(v1);
+    udskudslys(v1);
 
 
     //Barrier
+    push();
     stroke("black");
     strokeWeight(4);
     line(windowWidth/2, 0, windowWidth/2, windowHeight);
+    pop();
     
 }
