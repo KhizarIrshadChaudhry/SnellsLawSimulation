fab99479d796b42d026a66b72f94183e991a204b
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2025-03-11 09:18:43 +0100

    Tilføjet skabalon for P5.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    background(186, 183, 175);
+}
+
+
+function draw(){
+    stroke("black")
+    line(windowWidth/2, 0, windowWidth/2, windowHeight)
+    
+}
